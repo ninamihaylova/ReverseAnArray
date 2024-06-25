bb19fe550aee2d99eb393397b70b5dfc7d0dd717
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-25 16:23:01 +0300

    Initial commit

---
+++ ReverseAnArray/Program.cs
@@ -0,0 +1,18 @@
+﻿class  ReverseAnArray
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] nums = new int[n];
+
+        
+
+            for (int i=nums.Length-1; i>=0; i--)
+            {
+                nums[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(string.Join(" ",nums));
+                
+        }
+    }
+    
